24897079144d853b6d6a55889eb3581998f03ac0
dkhansemand <dkhansemand@gmail.com>
2017-03-11 20:16:53 +0100

    updated

--- admin_hifi/index.php
+++ admin_hifi/index.php
@@ -1,18 +1,16 @@
 <?php
-require_once './partials/header.php';
 
 if(!empty($_GET['p'])){
     $page = (string)$_GET['p'];
     if(file_exists('./pages/' . $page . '.php')){
-        $page = (string)$_GET['p'];
+        require_once './partials/header.php';
+        include './pages/' . $page . '.php';
     }else{
-        $page = 'dashboard';
+         header('Location: ../dashboard/', true);
     }
 }else{
-   $page = 'dashboard';
+    header('Location: ../dashboard/', true);
 }
-
-include './pages/' . $page . '.php';
 
 require_once './partials/footer.php';
 
