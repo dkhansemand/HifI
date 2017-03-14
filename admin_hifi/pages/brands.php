d98b89eb4ea10f619606a33fca8ba66146eeb723
dkhansemand <dkhansemand@gmail.com>
2017-03-14 19:54:09 +0100

    Brand update function added

--- admin_hifi/pages/brands.php
+++ admin_hifi/pages/brands.php
@@ -1,10 +1,7 @@
 <?php
-    $queryGetBrands = $conn->newQuery("SELECT bid, brandName FROM hifi_brands ORDER BY brandName ASC");
-    $queryGetBrands->execute();
-    $brands = $queryGetBrands->fetchAll(PDO::FETCH_ASSOC);
 
     if(!empty($_POST)){
-        if(!empty($_POST['brandName'])){
+        if(!empty($_POST['brandName']) && $_GET['option'] === 'Add'){
             $errCount = 0;
             $brandName = $_POST['brandName'];
             if(!preg_match('/\w+$/', $brandName)){
@@ -19,7 +16,7 @@
                     $success = true;
                     $successErr = false;
                     $successTitle = 'Mærke tilføjet';
-                    $successMsg = 'Mærket "' . $BrandName . '" er nu tilføjet til databasen';
+                    $successMsg = 'Mærket "' . $brandName . '" er nu tilføjet til databasen';
                     unset($brandName);
                 }
             }else{
@@ -29,7 +26,40 @@
                 $successMsg = 'Mærke navn skal udfyldes og må ikke have specialtegn.';
             }
         }
+
+         if(!empty($_POST['brandName']) && $_GET['option'] === 'Edit' && !empty($_GET['id'])){
+            $errCount = 0;
+            $bid = (int)$_GET['id'];
+            $brandName = $_POST['brandName'];
+            if(!preg_match('/\w+$/', $brandName)){
+                ++$errCount;
+                $errBrandName = 'Feltet må kun indholde bogstaver og tal.';
+            }
+
+            if($errCount === 0){
+                $queryUpdateBrand = $conn->newQuery("UPDATE hifi_brands SET brandName = :NAME WHERE bid = :ID");
+                $queryUpdateBrand->bindParam(':NAME', $brandName, PDO::PARAM_STR);
+                $queryUpdateBrand->bindParam(':ID', $bid, PDO::PARAM_INT);
+                if($queryUpdateBrand->execute()){
+                    $success = true;
+                    $successErr = false;
+                    $successTitle = 'Mærke Opdateret';
+                    $successMsg = 'Mærket "' . $brandName . '" er nu opdateret i databasen';
+                    unset($brandName);
+                }
+            }else{
+                $success = true;
+                $successErr = true;
+                $successTitle = 'Fejl i indtastning!';
+                $successMsg = 'Mærke navn skal udfyldes og må ikke have specialtegn.';
+            }
+
+         }
     }
+
+    $queryGetBrands = $conn->newQuery("SELECT bid, brandName FROM hifi_brands ORDER BY brandName ASC");
+    $queryGetBrands->execute();
+    $brands = $queryGetBrands->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <script>
 var validateBrand = {
@@ -47,7 +77,7 @@ var validateBrand = {
                                 .removeClass("glyphicon-remove")
                                 .addClass("glyphicon-ok")
                                 .next('.errMsg').html('');
-                return true;
+                
             }else{
                 $(inputField).parent('div')
                                 .removeClass("has-success")
@@ -57,7 +87,7 @@ var validateBrand = {
                                 .removeClass("glyphicon-ok")
                                 .addClass("glyphicon-remove")
                                 .next('.errMsg').html('Mærke navn skal udfyldes, og må ikke have specialtegn');
-                return false;
+                
             }
         }
 }
@@ -69,6 +99,13 @@ $(document).ready( () => {
         "use strict";
          if(objForm.target.name === "brandName"){
             validateBrand.brandname("#brandName");
+        }
+    });
+
+    $("#brandFormUpdate").keyup( (objForm) => {
+        "use strict";
+         if(objForm.target.name === "brandName"){
+            validateBrand.brandname("#brandNameU");
         }
     });
  
@@ -146,7 +183,7 @@ $(document).ready( () => {
                             </div>
                         </div>
                         <div class="panel-body">
-                           <form action="" method="post" id="brandForm">
+                           <form action="./index.php?p=Brands&option=Add" method="post" id="brandForm">
                                <div class="input-group has-feedback">
                                 <span class="input-group-addon" id="sizing-addon2">Mærke navn</span>
                                 <input type="text" class="form-control" placeholder="Mærke navn" name="brandName" id="brandName" value="<?=@$brandName?>" aria-describedby="sizing-addon2" required>
@@ -174,8 +211,38 @@ $(document).ready( () => {
                                 foreach($brands as $brand){
                             ?>
                             <tr>
-                                <td><?=$brand['brandName']?></td>
-                                <td><a href="./index.php?p=Brands&option=Edit&id=<?=$brand['bid']?>" class=" btn btn-info"><i class="fa fa-pencil"></i></a></td>
+                                <td>
+                                    <?=$brand['brandName']?>
+                                    <div class="collapse" id="brandCollapseId<?=$brand['bid']?>">
+                                        <div class="well">
+                                            <script>
+                                                $(document).ready( () => {
+                                                 $("#brandFormUpdate<?=$brand['bid']?>").keyup( (objForm) => {
+                                                    "use strict";
+                                                    if(objForm.target.name === "brandName"){
+                                                        validateBrand.brandname("#brandNameU<?=$brand['bid']?>");
+                                                    }
+                                                });
+                                                });
+                                            </script>
+                                           <form action="./index.php?p=Brands&option=Edit&id=<?=$brand['bid']?>" method="post" id="brandFormUpdate<?=$brand['bid']?>">
+                                            <div class="input-group has-feedback">
+                                                <span class="input-group-addon" id="sizing-addon2">Mærke navn</span>
+                                                <input type="text" class="form-control" placeholder="Mærke navn" name="brandName" id="brandNameU<?=$brand['bid']?>" value="<?=$brand['brandName']?>" aria-describedby="sizing-addon2" required>
+                                                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                                                <span class="errMsg alert-warning"><?=@$errBrandName?></span>
+                                            </div><br>
+                                            <button type="submit" name="btnEdit" class="btn btn-lg btn-info">Ret</button>
+                                        </form>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    
+                                    <button class="btn btn-info" type="button" data-toggle="collapse" data-target="#brandCollapseId<?=$brand['bid']?>" aria-expanded="false" aria-controls="brandCollapseId<?=$brand['bid']?>">
+                                        <i class="fa fa-pencil"></i>
+                                    </button>
+                                </td>
                                 <td><a href="./index.php?p=Brands&option=Delete&id=<?=$brand['bid']?>" class="btn btn-danger"><i class="fa fa-remove"></i></a></td>
                             </tr>
 
